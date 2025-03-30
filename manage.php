1cf5be503f7504e0b17a2fc1ab30a41c8c46c3e8
Snickser <snickser@gmail.com>
2025-03-30 09:49:34 +0300

    add

--- manage.php
+++ manage.php
@@ -233,7 +233,7 @@ if($filter != 'showarchived') {
             <div class="modal-dialog">
                 <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel' . $bank_entry->id . '">' . get_string('files') . '</h5>
+                    <h5 class="modal-title" id="staticBackdropLabel' . $bank_entry->id . '">' . get_string('files') . ' (' .count($files) . ')</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span></button>
                 </div>
                 <div class="modal-body">
