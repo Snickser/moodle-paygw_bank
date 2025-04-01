75979f12924cd8b0758fe90adc29985db2513d32
Snickser <snickser@gmail.com>
2025-04-01 09:41:02 +0300

    tune

--- manage.php
+++ manage.php
@@ -306,7 +306,7 @@ if($filter == 'showarchived' && has_capability('paygw/bank:managepayments', $con
             foreach ($files as $f) {
                 // $f is an instance of stored_file
                 $url = moodle_url::make_pluginfile_url($f->get_contextid(), $f->get_component(), $f->get_filearea(), $f->get_itemid(), $f->get_filepath(), $f->get_filename(), false);
-                $hasfilesbody .= '<li><a href="' . $url . '" download><b>' . $f->get_filename() . '</b></a><br>';
+                $hasfilesbody .= '<li class="mb-2"><a href="' . $url . '" download><b>' . $f->get_filename() . '</b></a><br>';
             	$hasfilesbody .= get_string('size').': '. round($f->get_filesize()/1024,2) . ' KB</li>';
                 if (str_ends_with($f->get_filename(), ".png") || str_ends_with($f->get_filename(), ".jpeg") || str_ends_with($f->get_filename(), ".jpg") || str_ends_with($f->get_filename(), ".gif")) {
                     $hasfilesimg .= "<p align=center class='pt-3'><img class='rounded shadow' style='max-width: 100%; object-fit: contain;' src='$url'></p>";
