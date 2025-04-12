f795ce0c36fc346b11c23817ed7fcac097c44b1e
Snickser <snickser@gmail.com>
2025-04-12 14:24:11 +0300

    fix

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 3025040800;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 3025041200;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051100;        // Requires this Moodle version.
 $plugin->component = 'paygw_bank';      // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '2.0';
+$plugin->release   = '2.0.32';
