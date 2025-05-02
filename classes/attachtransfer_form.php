65e23ad714bec48dde7818c0cdb27edfce88b5e3
Snickser <snickser@gmail.com>
2025-05-02 20:41:26 +0300

    fix

--- classes/attachtransfer_form.php
+++ classes/attachtransfer_form.php
@@ -37,7 +37,10 @@ require_once($CFG->libdir . '/formslib.php');
  */
 class attachtransfer_form extends \moodleform {
 
+    /** @var string */
     public $userfile;
+
+    /** @var string */
     public $editfiles;
 
     /**
