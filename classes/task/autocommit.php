92ab468514e548644273f3360d19141570b578df
Snickser <snickser@gmail.com>
2025-06-16 09:11:07 +0300

    fix

--- classes/task/autocommit.php
+++ classes/task/autocommit.php
@@ -55,12 +55,11 @@ class autocommit extends \core\task\scheduled_task {
         $items = bank_helper::get_pending('P');
 
         foreach ($items as $item) {
-
             $config = (object) helper::get_gateway_configuration($item->component, $item->paymentarea, $item->itemid, 'bank');
 
-            if ($config->autodeny > 0 && time()-$config->autodeny > $item->timecreated) {
+            if ($config->autodeny > 0 && time() - $config->autodeny > $item->timecreated) {
                 mtrace($item->id . ' declined');
-        	bank_helper::deny_pay($item->id);
+                bank_helper::deny_pay($item->id);
             }
 
             if (!$item->hasfiles) {
